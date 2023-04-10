6fa28d73ac035ce83be6f16a8a838715f8400827
littlehui <>
2023-04-11 01:00:46 +0800

    ss

--- dzt-web/dzt-app-web/src/main/java/com/idaymay/dzt/app/web/config/OpenAiConfig.java
+++ dzt-web/dzt-app-web/src/main/java/com/idaymay/dzt/app/web/config/OpenAiConfig.java
@@ -3,8 +3,11 @@ package com.idaymay.dzt.app.web.config;
 import com.idaymay.dzt.bean.openai.OpenAiConfigSupport;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
 
 /**
  * TODO
@@ -13,6 +16,7 @@ import org.springframework.context.annotation.Configuration;
  * @version 1.0
  * @date 2023/04/10 23:22
  */
+@Slf4j
 @Configuration
 @ConfigurationProperties(prefix = "config.openai")
 public class OpenAiConfig implements OpenAiConfigSupport {
@@ -20,6 +24,11 @@ public class OpenAiConfig implements OpenAiConfigSupport {
     @Getter
     @Setter
     private String apiKey;
+
+    @PostConstruct
+    public void openAiConfigCreated() {
+        log.info("openAiConfig created!");
+    }
 
     @Override
     public String getApiKey() {
